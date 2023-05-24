0d6623fda0c83acb42e81c00e8e373c0b7fc96a1
ernie-h <35308050+ernie-h@users.noreply.github.com>
2023-05-23 17:07:02 -0700

    update tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,35 @@
-import prisma from '../lib/prisma'
+import prisma from "../lib/prisma";
 
 async function main() {
   const response = await Promise.all([
     prisma.users.upsert({
-      where: { email: 'rauchg@vercel.com' },
       update: {},
       create: {
-        name: 'Guillermo Rauch',
-        email: 'rauchg@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg',
+        name: "Ernie Hao",
       },
     }),
     prisma.users.upsert({
-      where: { email: 'lee@vercel.com' },
       update: {},
       create: {
-        name: 'Lee Robinson',
-        email: 'lee@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg',
+        name: "Alex Beers",
       },
     }),
-    await prisma.users.upsert({
-      where: { email: 'stey@vercel.com' },
+    prisma.users.upsert({
+      // where: { email: "rauchg@vercel.com" },
       update: {},
       create: {
-        name: 'Steven Tey',
-        email: 'stey@vercel.com',
-        image:
-          'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg',
+        name: "Sheriff Hao",
       },
     }),
-  ])
-  console.log(response)
+  ]);
+  console.log(response);
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
